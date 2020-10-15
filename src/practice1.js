cb3851cc3c8fb981ed3a481f382d29bc8d789995
JINZHANG2017 <zj574406254@hotmail.com>
2020-10-15 20:32:41 +0800

    [ZhangJin] feat: finish exercise 1

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,16 @@
-const parseData
+
+function parseData(input){
+    const output=[];
+    for(let dataItem of input.data){	
+        // console.log(item);
+        let outputItem={};
+        for(let value of dataItem ){
+            for(let keyItem of input.column){
+                outputItem[keyItem.name]=value;
+            }
+        }
+        output.push(outputItem);
+    }
+    return output;
+}
 export { parseData };
